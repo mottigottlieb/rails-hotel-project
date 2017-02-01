a0327d1566f30e13f0e4b5f6f609b2f153328388
Motti <=>
2017-02-01 16:13:27 -0500

    added omniauth provider to User model

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,7 +248,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+  config.omniauth :facebook, '363989027321284', 'bb9f5248135dd0506bf6940832d5368c'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
